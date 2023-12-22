acf18b5abce1dfbd8629d3cc14f85cb6786a53dc
tsvtln <tsvtln@abv.bg>
2023-12-22 14:17:44 +0200

    у

--- event_clicker.py
+++ event_clicker.py
@@ -1,11 +1,103 @@
 import pyautogui
+import os
+from PIL import Image
+from skimage.metrics import structural_similarity as ssim
+import numpy as np
+import time
 
-# Specify the pixel coordinates where you want to click
-x_coordinate = 100
-y_coordinate = 200
+time.sleep(5)
 
-# Move the mouse to the specified coordinates
-pyautogui.moveTo(x_coordinate, y_coordinate)
+targets = []
 
-# Perform a mouse click (left-click)
-pyautogui.leftClick()
+# Take a screenshot
+working_dir = os.path.dirname(os.path.abspath(__file__))
+target_directory = 'screenshots'
+level_screen = 'level_screen.png'
+screenshot_dir = os.path.join(working_dir, target_directory)
+if not os.path.exists(screenshot_dir):
+    os.makedirs(screenshot_dir)
+
+screenshot = pyautogui.screenshot()
+screenshot.save(os.path.join(screenshot_dir, level_screen))
+
+"""
+img info
+3ple image/target
+IMG1 -> X 794 / Y 318 / W 87 / H 75
+IMG2 -> X 900 / Y 319 / W 87 / H 75
+IMG3 -> X1003 / Y 318 / W 87 / H 75
+"""
+
+
+# Set targets from the screenshot
+def extract_targets(screenshot_path, td):  # td = targets dir
+    # Load the screenshot
+    screenshot_path = os.path.join(screenshot_path, 'level_screen.png')
+    scns = Image.open(screenshot_path)  # scns = screenshot
+    target_regions = [
+        (796, 318, 881, 393),  # Example: (left, top, right, bottom)
+        (900, 319, 987, 394),
+        (1003, 318, 1090, 393)
+    ]
+    # Extract and save target images
+    for i, region in enumerate(target_regions):
+        target_image = scns.crop(region)
+        target_image_path = os.path.join(td, f'target_{i + 1}.png')
+        target_image.save(target_image_path)
+
+
+# Extract targets
+targets_directory = 'targets'
+targets_path = os.path.join(working_dir, targets_directory)
+if not os.path.exists(targets_path):
+    os.makedirs(targets_path)
+extract_targets(screenshot_dir, targets_path)
+
+
+def compare_targets_with_to_find(target_path, to_find_directory):
+    global targets
+    targets = []
+    target_name = ''
+    for i in range(1, 4):
+        # Load the target image
+        tp = os.path.join(target_path, f"target_{i}.png")
+        target_image = Image.open(tp)
+
+        # Loop through images in the to_find directory
+        for filename in os.listdir(to_find_directory):
+            image_path = os.path.join(to_find_directory, filename)
+
+            # Load the image from to_find directory
+            to_find_image = Image.open(image_path)
+
+            # Resize the target image to match the dimensions of the to_find image
+            target_image_resized = target_image.resize(to_find_image.size)
+
+            # Convert images to numpy arrays
+            target_np = np.array(target_image_resized)
+            to_find_np = np.array(to_find_image)
+
+            # Calculate Structural Similarity Index (SSI) with a smaller win_size
+            smaller_side = min(target_np.shape[0], target_np.shape[1], to_find_np.shape[0], to_find_np.shape[1])
+            win_size = min(smaller_side, 3)  # Set to 5 or any odd value less than or equal to the smaller side
+            similarity_index, _ = ssim(target_np, to_find_np, win_size=win_size, full=True)
+
+            # Print the similarity index for each image
+            print(f"Similarity index with {filename}: {similarity_index}")
+
+            # You can store the results in the 'targets' list if needed
+            targets.append((f"target_{i}.png", filename, similarity_index))
+
+    return targets
+
+
+# Compare targets to images in the to_find directory
+imgs_comp_directory = 'imgs_comp'
+to_find_directory = 'to_find'
+to_find_path = os.path.join(working_dir, imgs_comp_directory, to_find_directory)
+
+result_targets = compare_targets_with_to_find(targets_path, to_find_path)
+
+# Example: Print the results
+for result in result_targets:
+    print(f"Comparison between {result[0]} and {result[1]}: {result[2]}")
