4cc965e9caa4b8eb033bd18870a7ea4d5d3da70a
tsvtln <tsvtln@abv.bg>
2023-12-22 12:15:55 +0200

    u

---
+++ event_clicker.py
@@ -0,0 +1,11 @@
+import pyautogui
+
+# Specify the pixel coordinates where you want to click
+x_coordinate = 100
+y_coordinate = 200
+
+# Move the mouse to the specified coordinates
+pyautogui.moveTo(x_coordinate, y_coordinate)
+
+# Perform a mouse click (left-click)
+pyautogui.leftClick()
